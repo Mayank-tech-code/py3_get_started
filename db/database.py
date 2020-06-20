eed11248ed0e52b5d84f34146e2e433c6ede72ca
unknown <vinapanandikar@gmail.com>
2020-06-21 00:44:25 +0530

    one to many relationship and inner join added...

--- db/database.py
+++ db/database.py
@@ -8,6 +8,9 @@ connection = psycopg2.connect(user = "postgres",
 
 cursor = connection.cursor()
 
+cursor.execute("truncate college cascade")
+connection.commit()
+
 name = 'DY Patil'
 address = 'Pune'
 cursor.execute('insert into college(name, address) values(%s, %s)',(name, address))
@@ -20,6 +23,24 @@ connection.commit()
 
 cursor.execute('select * from college')
 rows = cursor.fetchall()
+college_ids = []
+for row in rows:
+    print(row)
+    college_ids.append(row[0])
+
+name = 'prakash'
+college_id = college_ids[0]
+cursor.execute('insert into student(name, college_id) values(%s, %s)',(name, college_id))
+
+name = 'nisar'
+college_id = college_ids[1]
+cursor.execute('insert into student(name, college_id) values(%s, %s)',(name, college_id))
+
+connection.commit()
+
+cursor.execute('select * from college c inner join student s on c.id = s.college_id')
+rows = cursor.fetchall()
+college_ids = []
 for row in rows:
     print(row)
 
