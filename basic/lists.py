89158f75bc34a64b4b53af8620398e1b88e85d06
unknown <vinapanandikar@gmail.com>
2020-06-20 23:09:36 +0530

    additional changes based on feedback in the session. e.g. insert an element in the list

--- basic/lists.py
+++ basic/lists.py
@@ -1,11 +1,11 @@
 names = ['prakash','rohit','adinath']
-print("Printing names list",names)
+print("Printing names list: ",names)
 
 for name in names:
     print('Print name in first for loop: ',name)
 
 for i in range(0,len(names)):
-    print('Print name in second for loop: ',name)
+    print('Print name in second for loop: ',names[i])
 
 names.append('nisar')
 names.append('divya')
@@ -15,4 +15,10 @@ del names[0]
 print(names)
 
 names.pop()
+print(names)
+
+names[0] = "vishal"
+print(names)
+
+names.insert(1,"vinay")
 print(names)
